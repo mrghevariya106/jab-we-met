a01c29e0604a1df53bd688f878f12e0b3c846c19
MAYUR JITUBHAI GHEVARIYA <50697840+mrghevariya106@users.noreply.github.com>
2021-12-04 21:07:38 +0530

    integration continue

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const intsance = axios.create({
-  baseURL: "http://localhost:8001",
+  baseURL: "https://jab-we-met-backend.herokuapp.com/",
 });
 
 export default intsance;
