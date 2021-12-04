9e88de78ec5df83003c7bc04d1a51fcffa5ca221
MAYUR JITUBHAI GHEVARIYA <50697840+mrghevariya106@users.noreply.github.com>
2021-12-04 17:01:39 +0530

    backend-frontend integration

--- src/JWMCards.js
+++ src/JWMCards.js
@@ -1,27 +1,21 @@
-import React, { useState } from "react";
-import TinderCard from 'react-tinder-card'
+import React, { useState, useEffect } from "react";
+import TinderCard from "react-tinder-card";
 import "./JWMCards.css";
-
+import axios from "./axios.js";
 
 function JWMCards() {
-  const [people, setPeople] = useState([
-    {
-        name: "Tony Stark",
-        url: "https://images.hindustantimes.com/rf/image_size_630x354/HT/p2/2020/04/04/Pictures/_8e0b514c-7675-11ea-bbaf-88371e4f22eb.JPG",
-      },
-      {
-        name: "Thor",
-        url: "https://hips.hearstapps.com/digitalspyuk.cdnds.net/18/09/1519729389-thor-ragnarok-reviews-big.jpg?crop=1.00xw:0.923xh;0,0&resize=1200:*",
-      },
-      {
-        name: "Papper",
-        url: "https://s3.r29static.com/bin/entry/9d3/x,80/2174391/image.jpg",
-      },
-      {
-        name: "Glane",
-        url: "https://www.gizmodo.com.au/content/uploads/sites/2/2017/03/15/ro7mjdfntrspvxru4s1i.jpg",
-      }
-  ]);
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    console.log(axios)
+    async function fetchData() {
+      const req = await axios.get("/jwm/cards");
+      console.log(req.data)
+      setPeople(req.data);
+    }
+
+    fetchData();
+  }, []);
 
   const swiped = (direction, nameToDelete) => {
     console.log("Removing: " + nameToDelete);
@@ -44,7 +38,7 @@ function JWMCards() {
           >
             <div
               className="card"
-              style={{ backgroundImage: `url(${person.url})` }}
+              style={{ backgroundImage: `url(${person.imgUrl})` }}
             >
               <h3>{person.name}</h3>
             </div>
